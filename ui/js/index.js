2eb2aeaecf88c1aad802fa9a149c1785727d41a3
luoruofeng <ruofeng.luo@dmall.com>
2021-08-06 11:47:06 +0800

    page and wsserver

--- ui/js/index.js
+++ ui/js/index.js
@@ -8,48 +8,71 @@ function scrollBottom(){
     $('.message-body').eq(0).animate({ scrollTop: $('.message-body').eq(0).prop("scrollHeight")}, 1000);
 }
 
-var ws = new WebSocket("ws://127.0.0.1:5555/echo");
 
-ws.onmessage = function (res) {
-    console.log(res.data);
+var roomnum = null;
+$(document).ready(function (){
+    scrollBottom();
+    roomnum = $("#room-num").val()
+    alert(roomnum);
+    ws = createWebSocket();
+})
 
-    var data = JSON.parse(res.data);
-    if (data.msg_type == 'message') {
-        nickname = data.nickname
+function createWebSocket(){
+    var ws = new WebSocket("ws://127.0.0.1:5555/echo/"+roomnum);
 
-        var messageheader = $("<div class=\"message-header\"></div>");
-        var time = $("<code></code>");
-        time.html(format(new Date()));
-        var messagetext = $("<div class=\"message-text\"></div>");
-        var messageitem = $("<div class=\"message-item\"></div>");
-        var messagecontent = $("<div class=\"message-content\"></div>");
+    ws.onmessage = function (res) {
+        console.log(res.data);
 
-        messageheader.append(nickname);
-        messageheader.append(time);
-        messagetext.html("content");
-        messageitem.append(messageheader);
-        messagecontent.append(messagetext);
-        messageitem.append(messagecontent);
-        $(".message-body").eq(0).append(messageitem);
+        var data = JSON.parse(res.data);
+        if (data.msg_type == 'message') {
+            nickname = data.nickname;
+            content = data.content;
 
-        scrollBottom();
+            var messageheader = $("<div class=\"message-header\"></div>");
+            var time = $("<code></code>");
+            time.html(format(new Date()));
+            var messagetext = $("<div class=\"message-text\"></div>");
+            var messageitem = $("<div class=\"message-item\"></div>");
+            var messagecontent = $("<div class=\"message-content\"></div>");
+
+            messageheader.append(nickname);
+            messageheader.append(time);
+            messagetext.html(content);
+            messageitem.append(messageheader);
+            messagecontent.append(messagetext);
+            messageitem.append(messagecontent);
+            $(".message-body").eq(0).append(messageitem);
+
+            scrollBottom();
+        }
+
+    };
+
+    ws.onopen = function (event){
+      // alert("WebSocket is open now.");
+    };
+
+    ws.onclose = function(event) {
+        var code = event.code;
+        var reason = event.reason;
+        var wasClean = event.wasClean;
     }
 
-};
-
-ws.onopen = function (event){
-  // alert("WebSocket is open now.");
-};
-
-ws.onclose = function(event) {
-    var code = event.code;
-    var reason = event.reason;
-    var wasClean = event.wasClean;
+    return ws;
 }
 
 function send_msg() {
     var msg = document.getElementById("message").value;
-    ws.send(msg);
+    if(msg == "" || msg == null){
+        return
+    }
+    obj = {
+        nickname:$("#nick-name").text(),
+        type:"message",
+        content: msg
+    }
+    ws.send(JSON.stringify(obj));
+    $("#message").val("");
      window.event.returnValue =false;//阻止表单跳转
 }
 
@@ -57,5 +80,4 @@ function copy_content(id) {
     var copyText = $("#"+id);//获取对象
     copyText.select();//选择
     document.execCommand("Copy");//执行复制
-    // alert("复制成功！");
 }
--- wsserver.py
+++ wsserver.py
@@ -3,24 +3,61 @@ from geventwebsocket import WebSocketServer, WebSocketApplication, Resource, Web
 from collections import OrderedDict
 import json
 
+class Config:
+    def __init__(self):
+        self.rooms = {} # key：房间号 value：client_array
+        self.members = {} # key:addr_tuple value：房间号
+
 class EchoApplication(WebSocketApplication):
+    def __init__(self,ws):
+        super().__init__(ws)
+
+    def addToRoom(self):
+        path_arr = self.ws.path.split("/")
+        room_number = path_arr[len(path_arr) - 1]
+        if room_number not in config.rooms:
+            config.rooms[room_number] = []
+        config.rooms[room_number].append(self.ws.handler.active_client)
+        config.members[self.ws.handler.active_client.address] = room_number
+
+
     def on_open(self):
+        self.addToRoom()
         print("Connection opened")
 
+    def getRoomNumByAddr(self):
+        return config.members[self.ws.handler.active_client.address]
+
     def on_message(self, message):
-        for addr,client in self.ws.handler.server.clients.items():
+        if message == None or message == "":
+            return
+        message_obj = json.loads(message.decode("utf-8"))
+        nickname = message_obj["nickname"]
+        mes_type = message_obj["type"]
+        content = message_obj["content"]
+        roomNum = self.getRoomNumByAddr()
+        for client in config.rooms[roomNum]:
             if client and client.ws and message:
                 client.ws.send(json.dumps({
-                    'msg_type': 'message',
-                    'nickname': message.decode("utf-8")
+                    'msg_type': mes_type,
+                    'nickname': nickname,
+                    "content" : content
                 }))
 
 
     def on_close(self, reason):
-        print(reason)
+        roomNum = self.getRoomNumByAddr()
+        config.members.pop(self.ws.handler.active_client.address)
+        config.rooms[roomNum].remove(self.ws.handler.active_client)
+
+        if len(config.rooms[roomNum]) == 0:
+            config.rooms.pop(roomNum)
+
+        print("close")
 
 if __name__ == '__main__':
+    config = Config()
     WebSocketServer(
         ('0.0.0.0', 5555),
-        Resource(OrderedDict([('/echo', EchoApplication)]))
+        Resource(OrderedDict([('/echo/.*', EchoApplication)]))
     ).serve_forever()
